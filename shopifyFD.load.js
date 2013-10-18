3957b0fb05b8e724cc1627b3adf4f2ec9cfe00ac
Jason Bowman <jason@freakdesign.com.au>
2013-10-18 15:39:27 +1000

    update file links

---
+++ shopifyFD.load.js
@@ -0,0 +1,32 @@
+/*
+
+
+
+      ############# 
+    ##            *## 
+   #        %      **#
+  #        %%%    ***#
+ #       %%F%D%%   ****#
+#          %%%    *****#
+#   ###     %     ###***# 
+#  # ####       #### #**# 
+#  #     #     #     #**# 
+#   #####  # #  #####***# 
+#         #   #  *******# 
+ ### #           **# ### 
+     # - - - - - - #                 
+      | | | | | | |
+	
+     freakdesign.com               
+
+
+Written by jason from freakdesign
+freakdesign.com.au
+shopify.freakdesign.com.au
+Don't be a tool and pass this off as your own. Contribute!
+
+*/
+var shopifyjs = document.createElement("script");
+shopifyjs.type = "text/javascript";
+shopifyjs.src = "//rawgithub.com/freakdesign/shopifyFD/master/shopifyFD.min.js";
+document.body.appendChild(shopifyjs);
