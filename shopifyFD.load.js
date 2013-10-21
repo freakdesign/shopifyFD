56cfc8f8d89cc31a8a90b154c9a44686123a08bf
Jason Bowman <jason@freakdesign.com.au>
2013-10-21 11:07:20 +1000

    Consistent naming

--- shopifyFD.load.js
+++ shopifyFD.load.js
@@ -26,7 +26,7 @@ shopify.freakdesign.com.au
 Don't be a tool and pass this off as your own. Contribute!
 
 */
-var shopifyjs = document.createElement("script");
-shopifyjs.type = "text/javascript";
-shopifyjs.src = "//rawgithub.com/freakdesign/shopifyFD/master/shopifyFD.min.js";
-document.body.appendChild(shopifyjs);
+var shopifyFD = document.createElement("script");
+shopifyFD.type = "text/javascript";
+shopifyFD.src = "//rawgithub.com/freakdesign/shopifyFD/master/shopifyFD.min.js";
+document.body.appendChild(shopifyFD);
