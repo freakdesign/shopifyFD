00e1491c16953d0277bb02b29605ee248275b1d9
Jason Bowman <jason@freakdesign.com.au>
2013-10-24 16:15:42 +1000

    remove Chome App

--- chrome_extension/shopifyFD/background.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  /* 
-  No tabs or host permissions needed!
-  */
-  chrome.tabs.executeScript({
-    code: 'var shopifyFD = document.createElement("script");shopifyFD.type = "text/javascript";shopifyFD.src = "//rawgithub.com/freakdesign/shopifyFD/master/shopifyFD.min.js";document.body.appendChild(shopifyFD);'
-  });
-});
